d87e9108d778ecc83a20959c560df5b90f6c9799
proudbird <v6825403@hotmail.com>
2019-04-09 18:48:33 +0200

    init

---
+++ public/src/js/offers.js
@@ -0,0 +1,229 @@
+const layout = {
+  rows: [{
+      view: "template",
+      template: "<h1>Прайс-лист</h1>",
+      height: 70
+    },
+    {
+      cols: [{
+          view: "tree",
+          //width: 250,
+          id: "Folders",
+          select: true,
+          gravity: 0.1,
+          data: []
+        },
+        {
+          view: "resizer"
+        },
+        {
+          view: "datatable",
+          id: "List",
+          //width: 700,
+          select: true,
+          gravity: 0.5,
+          columns: [{
+              id: "title",
+              header: "Наименование",
+              fillspace: true
+            },
+            {
+              id: "sku",
+              header: "Артикул",
+              width: 100,
+              css: {
+                'text-align': 'center'
+              }
+            },
+            {
+              id: "unit",
+              header: "Ед. изм.",
+              width: 80,
+              css: {
+                'text-align': 'center'
+              }
+            },
+            {
+              id: "rest",
+              header: "Остаток",
+              width: 80,
+              css: {
+                'text-align': 'right'
+              }
+            },
+            {
+              id: "price",
+              header: "Цена без НДС",
+              width: 100,
+              css: {
+                'text-align': 'right'
+              },
+              format: function (value) {
+                return webix.i18n.numberFormat(value)
+              }
+            },
+            {
+              id: "priceVAT",
+              header: "Цена с НДС",
+              width: 100,
+              css: {
+                'text-align': 'right'
+              },
+              format: function (value) {
+                return webix.i18n.numberFormat(value)
+              }
+            }
+          ],
+          data: []
+        },
+        {
+          view: "resizer"
+        },
+        {
+          gravity: 0.4,
+          rows: [{
+              view: "carousel",
+              id: "Thumbnail",
+              css: "thumbnail",
+              cols: []
+            },
+            {
+              view: "dataview",
+              id: "ImageList",
+              css: "nav_list",
+              yCount: 1, // one row
+              select: true, // item selection enabled
+              scroll: false, // non-scrollable
+              type: {
+                width: 100,
+                height: 65
+              },
+              template: img,
+              data: []
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+function img(obj) {
+  return "<div style='background-image: url(\"offers/img/" + obj.img + "\");' class='content'></div>";
+}
+
+function render() {
+  webix.ui(
+    layout
+  );
+
+  var offerId = window.location.pathname.replace("/","");
+
+  var folders = $$("Folders");
+  folders.load("folders?offerId=" + offerId, "json");
+
+  var list = $$("List");
+  list.load("items?offerId=" + offerId, "json");
+
+  folders.attachEvent("onAfterLoad", function () {
+    folders.openAll();
+    selectFirstItem(folders);
+  });
+
+  list.attachEvent("onAfterLoad", function () {
+    selectFirstItem(list);
+  });
+
+  var imageList = $$("ImageList");
+  
+  
+
+  var thumbnail = $$("Thumbnail");
+  thumbnail.attachEvent("onShow", function (id) {
+    imageList.select(id);
+  });
+
+  folders.attachEvent("onSelectChange", function () {
+    var item = folders.getSelectedItem(false);
+    const parents = [];
+    parents.push(item.id);
+    if (folders.isBranch(item.id)) {
+      folders.data.each(
+        function (obj) {
+          parents.push(obj.id);
+        }, this, true, item.id);
+    }
+    list.filter(function (obj) {
+      if(parents.includes(obj.parentId)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    selectFirstItem(list);
+  });
+
+  list.attachEvent("onSelectChange", function () {
+    var item = list.getSelectedItem(false);
+    if (item) {
+      imageList.filter(function (obj) {
+        return obj.itemId === item.id;
+      });
+    } 
+
+    const ids = [];
+    for (let i = 0; i < thumbnail._cells.length; i++) {
+      ids.push(thumbnail._cells[i].config.id);
+    }
+    for (let i = 0; i < ids.length; i++) {
+      try {
+        thumbnail.removeView(ids[i]);
+      } catch (err) {
+        //console.log(err);
+      }
+    }
+    imageList.clearAll();
+    if (item) {
+      imageList.load("images?offerId=" + offerId + "&itemId=" + item.id, "json", function() {
+        fillImageList(imageList, thumbnail);
+        selectFirstItem(imageList);
+        showThumbnail(imageList);
+      });
+    }
+  });
+
+  imageList.attachEvent("onSelectChange", function () {
+    showThumbnail(imageList);
+  });
+}
+
+function selectFirstItem(list) {
+  const firstId = list.getFirstId();
+  if (firstId) {
+    list.select(firstId);
+  } else {
+    console.log(list.config.id);
+  }
+}
+
+function showThumbnail(imageList) {
+  const item = imageList.getSelectedItem(false);
+  if(item) {
+    const cell = $$(item.id);
+    if(cell) {
+      cell.show();
+    }
+  }
+}
+
+function fillImageList(imageList, thumbnail) {
+  imageList.data.each(function (data) {
+    thumbnail.addView({
+      id: data.id,
+      css: "image",
+      template: img,
+      data: webix.copy(data)
+    });
+  });
+}
---
+++ server.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var router  = module.exports = express();
+var http    = require("http");
+var server  = http.createServer(router);
+var fs      = require('fs');
+const path  = require("path");
+const parser = require("csvtojson");
+const _ = require("lodash");
+
+router.use(express.static('./public'))
+router.use(express.json());     
+
+server.listen(process.env.PORT || 51051, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("e-offers server listening at", addr.address + ":" + addr.port);
+});
+
+router.get('/', function(req, res, next) {
+  res.send("offers");
+});
+
+router.get('/:param', async function(req, res, next) {
+  var result = [];
+  var data = [];
+  var map = {};
+  const offerId = req.query.offerId;
+  switch (req.params.param) {
+    case "folders":
+      fileName = path.join("./public", "offers", offerId, "folders.csv");
+      if(fs.existsSync(fileName)) {
+        data = await parser({ delimiter: "|" }).fromFile(fileName);
+        map = { id: "id", parentId: "parentId", title: "value" }
+        result = makeHierarchical(data, "parentId", "data", map);
+      }
+      result = {
+        id: "all-items",
+        value: "Все",
+        data: result
+      };
+      result = { data: result };
+      break;
+    case "items":
+      fileName = path.join("./public", "offers", offerId, "items.csv");
+      if(fs.existsSync(fileName)) {
+        result = await parser({ delimiter: "|" }).fromFile(fileName);
+      }
+      result = { data: result };
+      break;
+    case "images":
+      fileName = path.join("./public", "offers", offerId, "items.csv");
+      if(fs.existsSync(fileName)) {
+        result = await parser({ delimiter: "|" }).fromFile(path.join("./public", "offers", offerId, "images.csv"));
+      }
+      result = _.filter(result, { 'itemId': req.query.itemId });
+      result = { data: result};
+      break;
+    default:
+      if(fs.existsSync(path.join("./public/offers", req.params.param))) {
+        result = fs.readFileSync("index.html", "UTF-8");
+      } else {
+        result = "offers";
+      }
+  }
+  res.send(result);
+});
+
+function makeHierarchical(arr, parent, child, map) {
+  var tree = [],
+    mappedArr = {},
+    arrElem,
+    mappedElem;
+
+  // First map the nodes of the array to an object -> create a hash table.
+  for (var i = 0, len = arr.length; i < len; i++) {
+    arrElem = arr[i];
+    mappedArr[arrElem.id] = arrElem;
+    mappedArr[arrElem.id][child] = [];
+  }
+
+  for (var id in mappedArr) {
+    if (mappedArr.hasOwnProperty(id)) {
+      mappedElem = mappedArr[id];
+      if(map) {
+        const data = {};
+        for(let key in mappedElem) {
+          if(map[key]) {
+            const newKey = map[key];
+            data[newKey] = mappedElem[key];
+          } else {
+            data[key] = mappedElem[key];
+          }
+        }
+        mappedElem = data;
+      }
+      // If the element is not at the root level, add it to its parent array of children.
+      if (mappedElem[parent] && parent != "id") {
+        mappedArr[mappedElem[parent]][child].push(mappedElem);
+      }
+      // If the element is at the root level, add it to first level elements array.
+      else {
+        tree.push(mappedElem);
+      }
+    }
+  }
+  return tree;
+}
